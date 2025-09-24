65f079af922e2522b2ba8c4e57456898f6e4e285
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-09-24 14:44:52 +0200

    Prepare constants for joblib

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -6,3 +6,9 @@ TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
 
 # For cleaned data - use the same package location
 CLEAN_TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing_clean.csv")
+
+# Trained Model Artifacts
+MODEL_PATH = files("taxipred").joinpath("models/taxi_price_model.joblib")
+ENCODERS_PATH = files("taxipred").joinpath("models/encoders.joblib") 
+METRICS_PATH = files("taxipred").joinpath("models/model_metrics.joblib")
+FEATURE_IMPORTANCE_PATH = files("taxipred").joinpath("models/feature_importance.joblib")
